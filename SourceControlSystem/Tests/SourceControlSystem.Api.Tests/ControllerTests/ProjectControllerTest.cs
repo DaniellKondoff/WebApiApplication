0ab0cafbdef2d122312d7755840d75d1119bb592
Daniel Kondov <danielkondov91@gmail.com>
2017-08-28 23:56:30 +0300

    Unit Tests

---
+++ SourceControlSystem/Tests/SourceControlSystem.Api.Tests/ControllerTests/ProjectControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SourceControlSystem.Api.Controllers;
+using SourceControlSystem.Api.Models.Projects;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace SourceControlSystem.Api.Tests.ControllerTests
+{
+    [TestClass]
+    public class ProjectControllerTest
+    {
+        [TestMethod]
+        public void PostShouldValidateModelState()
+        {
+            var controller = new ProjectsController(TestObjectFactory.GetProjecService());
+            controller.Configuration = new HttpConfiguration();
+            var model = TestObjectFactory.GetInvalidModel();
+
+            controller.Validate(model);
+
+            var result = controller.Post(model);
+
+            Assert.IsFalse(controller.ModelState.IsValid);
+        }
+
+        [TestMethod]
+        public void PostShouldReturnBadRequestWithInvalidModel()
+        {
+            var controller = new ProjectsController(TestObjectFactory.GetProjecService());
+
+            controller.Configuration = new HttpConfiguration();
+
+            var model = TestObjectFactory.GetInvalidModel();
+
+            controller.Validate(model);
+
+            var result = controller.Post(model);
+
+            Assert.AreEqual(typeof(InvalidModelStateResult), result.GetType());
+        }
+    }
+}
---
+++ SourceControlSystem/Tests/SourceControlSystem.Api.Tests/TestObjectFactory.cs
@@ -0,0 +1,32 @@
+﻿using SourceControlSystem.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using SourceControlSystem.Api.Models.Projects;
+
+namespace SourceControlSystem.Api.Tests
+{
+    public static class TestObjectFactory
+    {
+        public  static IProjectService GetProjecService()
+        {
+            var projectService = new Mock<IProjectService>();
+            projectService.Setup(pr => pr.Add(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<bool>()
+                )).Returns(1);
+
+            return projectService.Object;
+        }
+
+        public static SaveProjectRequestModel GetInvalidModel()
+        {
+            return new SaveProjectRequestModel { Description = "TestDescription" };
+        }
+    }
+}
