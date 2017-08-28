3d046d46e69f63c14d26fd9825ee516496c6e516
Daniel Kondov <danielkondov91@gmail.com>
2017-08-28 23:56:20 +0300

    Constants Layer

---
+++ SourceControlSystem/SourceControlSystem.Common/Constants/Assemblies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControlSystem.Common.Constants
+{
+    public class Assemblies
+    {
+        public const string DataServices = "SourceControlSystem.Services.Data";
+        public const string WebApi = "SourceControlSystem.Api";
+    }
+}
---
+++ SourceControlSystem/SourceControlSystem.Common/Constants/ValidationConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceControlSystem.Common.Constants
+{
+    public class ValidationConstants
+    {
+        public const int MaxProjectName = 100;
+        public const int MaxProjectDescription = 100;
+    }
+}
